96440a465e10b0a0cd78d4ffb83be78bb365f5e6
genioustar <genioustar@gmail.com>
2023-02-20 23:34:33 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default () => (
+  <div className="w-full mx-auto max-w-xl mt-16">
+    <div className="flex justify-center ">
+      <h1 className="text-lime-500 font-bold text-4xl">Mini Tweets Login</h1>
+    </div>
+    <div>
+      <label>email</label>
+      <input className="border"></input>
+    </div>
+  </div>
+);
---
+++ pages/log-in.tsx
@@ -0,0 +1,18 @@
+export default function Login() {
+  return (
+    <div className="w-full mx-auto max-w-xl mt-16">
+      <div className="flex justify-center ">
+        <h1 className="text-lime-500 font-bold text-4xl">Mini Tweets Login</h1>
+      </div>
+      <div className="flex flex-col item-center">
+        <label htmlFor="email">email</label>
+        <input id="email" className="border"></input>
+        <label htmlFor="password">비밀번호</label>
+        <input id="password" className="border"></input>
+        <label htmlFor="confirm">비밀번호 확인</label>
+        <input id="confirm" className="border"></input>
+        <button>회원 가입</button>
+      </div>
+    </div>
+  );
+}
