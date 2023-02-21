a8e172c481aeee50dd819d0ec96b0199161b4cdd
genioustar <4574gkdl>
2023-02-22 08:45:35 +0900

    index페이지 UI 완성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,60 @@
 export default () => (
   <div className="w-full mx-auto max-w-xl mt-16">
-    <div className="flex justify-center items-center">
-      <div className="border bg-gray-800 w-20 h-20 border-gray-800 rounded-full"></div>
-      <div className="pl-4 space-y-2">
-        <p>나는 니꼬</p>
-        <p> 내가 이랬다가 저랬다가 어쩌구 저쩌구 했는데 어쩔티비 저쩔티비</p>
+    <div className="flex flex-col">
+      <div className="border">
+        <div className="flex justify-start items-center pl-2">
+          <div className="border my-2 bg-gray-800 w-20 h-20 border-gray-800 rounded-full"></div>
+          <div className="pl-4 space-y-2">
+            <p>나는 니꼬</p>
+            <p>
+              {" "}
+              내가 이랬다가 저랬다가 어쩌구 저쩌구 했는데 어쩔티비 저쩔티비
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center space-x-2">
+          <div className="flex items-center space-x-0.5 text-sm text-gray-600">
+            <svg
+              className="h-4 w-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+            <span>1</span>
+          </div>
+          <div className="flex items-center space-x-0.5 text-sm text-gray-600">
+            <svg
+              className="h-4 w-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+              ></path>
+            </svg>
+            <span>2</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-start items-center border pl-2">
+        <div className="border my-2 bg-gray-800 w-20 h-20 border-gray-800 rounded-full"></div>
+        <div className="pl-4 space-y-2">
+          <p>나는 니꼬</p>
+          <p> 내가 이랬다가 저랬다가 어쩌구 저쩌구 했는데 어쩔티비 저쩔티비</p>
+        </div>
       </div>
     </div>
     <div className="fixed bottom-5 right-5">
