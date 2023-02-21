ca9200e3c52424fedc0f71332f1b46e12c39d701
genioustar <genioustar@gmail.com>
2023-02-21 23:50:37 +0900

    tweets만 남고 ui 해결

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,18 +1,46 @@
 export default function CreateAccount() {
   return (
     <div className="w-full mx-auto max-w-xl mt-16">
-      <div className="flex justify-center ">
-        <h1 className="text-lime-500 font-bold text-4xl">Mini Tweets Login</h1>
-      </div>
-      <div className="flex flex-col item-center">
-        <label htmlFor="email">email</label>
-        <input id="email" className="border"></input>
-        <label htmlFor="password">비밀번호</label>
-        <input id="password" className="border"></input>
-        <label htmlFor="confirm">비밀번호 확인</label>
-        <input id="confirm" className="border"></input>
-        <button>회원 가입</button>
-      </div>
+      <h1 className="text-center text-lime-500 font-bold text-4xl">
+        Mini Tweets Login
+      </h1>
+      <form className="flex flex-col item-center mt-8 space-y-2">
+        <label htmlFor="email" className="font-medium text-gray-700">
+          Email Address
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="이메일 입력"
+          required
+        />
+        <label htmlFor="password" className="font-medium text-gray-700">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="비밀번호 입력"
+          required
+        ></input>
+        <label htmlFor="confirm" className="font-medium text-gray-700">
+          비밀번호 확인
+        </label>
+        <input
+          type="password"
+          id="confirm"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="비밀번호 재입력"
+          required
+        ></input>
+        <div>
+          <button className="border border-gray-300 bg-gray-50 text-gray-500 w-full mt-4 rounded-md py-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-700 focus:ring-white">
+            회원 가입
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,17 +1,37 @@
 export default function Login() {
   return (
     <div className="w-full mx-auto max-w-xl mt-16">
-      <div className="flex justify-center ">
-        <h1 className="text-lime-500 font-bold text-4xl">Mini Tweets Login</h1>
-      </div>
-      <div className="flex flex-col item-center">
-        <label htmlFor="email">email</label>
-        <input id="email" className="border"></input>
-        <label htmlFor="password">비밀번호</label>
-        <input id="password" className="border"></input>
+      <h1 className="text-center text-lime-500 font-bold text-4xl">
+        Mini Tweets Login
+      </h1>
+      <form className="flex flex-col item-center mt-8 space-y-2">
+        <label htmlFor="email" className="font-medium text-gray-700">
+          Email Address
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="이메일 입력"
+          required
+        />
+        <label htmlFor="password" className="font-medium text-gray-700">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="비밀번호 입력"
+          required
+        ></input>
 
-        <button>로그인</button>
-      </div>
+        <div className="font-bold text-xl">
+          <button className="border border-gray-300 bg-gray-50 text-gray-500 w-full mt-4 rounded-md py-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-700 focus:ring-white">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
