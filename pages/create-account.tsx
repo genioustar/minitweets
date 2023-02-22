43d1584374d50b45cf151a83ff0e9a26b90019b1
genioustar <4574gkdl>
2023-02-22 15:54:57 +0900

    디비 스키마 확정 create-account 페이지에서 비밀번호 확인하는 use-form-hook 까지 완성 테스트 필요

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,10 +1,25 @@
+import { useForm } from "react-hook-form";
+
 export default function CreateAccount() {
+  interface EnterForm {
+    email: string;
+    name: string;
+    password: string;
+    confirm: string;
+  }
+  const { register, watch, handleSubmit } = useForm<EnterForm>();
+  const onValid = (data: EnterForm) => {
+    console.log(data);
+  };
   return (
     <div className="w-full mx-auto max-w-xl mt-16">
       <h1 className="text-center text-lime-500 font-bold text-4xl">
         Mini Tweets Login
       </h1>
-      <form className="flex flex-col item-center mt-8 space-y-2">
+      <form
+        className="flex flex-col item-center mt-8 space-y-2"
+        onSubmit={handleSubmit(onValid)}
+      >
         <label htmlFor="email" className="font-medium text-gray-700">
           Email Address
         </label>
@@ -13,6 +28,18 @@ export default function CreateAccount() {
           id="email"
           className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
           placeholder="이메일 입력"
+          {...register("confirm", { required: true })}
+          required
+        />
+        <label htmlFor="name" className="font-medium text-gray-700">
+          Nic Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
+          placeholder="Nic Name 입력"
+          {...(register("name"), { required: true })}
           required
         />
         <label htmlFor="password" className="font-medium text-gray-700">
@@ -23,6 +50,7 @@ export default function CreateAccount() {
           id="password"
           className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
           placeholder="비밀번호 입력"
+          {...register("confirm", { required: true })}
           required
         ></input>
         <label htmlFor="confirm" className="font-medium text-gray-700">
@@ -33,6 +61,14 @@ export default function CreateAccount() {
           id="confirm"
           className="rounded-md border border-gray-300 bg-gray-50 px-3 text-gray-500 transition-color duration-200"
           placeholder="비밀번호 재입력"
+          {...register("confirm", {
+            required: true,
+            validate: (val: string) => {
+              if (watch("password") != val) {
+                return "Your passwords do no match";
+              }
+            },
+          })}
           required
         ></input>
         <div>
--- pages/tweet/[id].tsx
+++ pages/tweet/[id].tsx
@@ -1,21 +1,21 @@
 export default function DetailTweet() {
   return (
     <div className="w-full mx-auto max-w-xl mt-16">
-      <div className="flex justify-center items-center">
+      <div className="flex justify-start items-center border-b py-4 pl-4">
         <div className="border bg-gray-800 w-20 h-20 border-gray-800 rounded-full"></div>
         <div className="pl-4 space-y-2">
-          <p>나는 니꼬</p>
+          <p className="text-lg font-bold">나는 니꼬</p>
           <p> 내가 이랬다가 저랬다가 어쩌구 저쩌구 했는데 어쩔티비 저쩔티비</p>
         </div>
       </div>
-      <div className="">
+      <div className="pt-2 border-b flex space-x-4 justify-center item-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth="1.5"
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-10 h-10"
         >
           <path
             strokeLinecap="round"
@@ -28,9 +28,23 @@ export default function DetailTweet() {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-6 h-6"
+          className="w-10 h-10"
         >
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+        </svg>
+        <svg
+          className="h-10 w-10"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+          ></path>
         </svg>
       </div>
     </div>
