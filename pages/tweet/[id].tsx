c42d9901a416b96e5e3d6985bcd9a8c98059e57a
genioustar <4574gkdl>
2023-02-23 22:23:12 +0900

    tweets uri 변경

--- pages/tweet/[id].tsx
+++ pages/tweet/[id].tsx
@@ -1,7 +1,9 @@
+import { useRouter } from "next/router";
 import useSWR from "swr";
 
 export default function DetailTweet() {
-  const { data } = useSWR(`/api/tweets`);
+  const router = useRouter();
+  const { data } = useSWR(`/api/tweets/${router.query.id}`);
   return (
     <div className="w-full mx-auto max-w-xl mt-16">
       <div className="flex justify-start items-center border-b py-4 pl-4">
@@ -34,20 +36,6 @@ export default function DetailTweet() {
           className="w-10 h-10"
         >
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-        </svg>
-        <svg
-          className="h-10 w-10"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-          ></path>
         </svg>
       </div>
     </div>
